255952d9470a7610deb9cd3c21037f03a1b73740
Danilo <danilo.nascimento2@hotmail.com>
2022-09-06 20:14:32 +0200

    UserRepository methods implemented

--- src/repositories/UserRepositore.js
+++ src/repositories/UserRepositore.js
@@ -5,8 +5,36 @@ export class UserRepository {
         this.#lStorage = new Storage('user');
     }
 
-    async save(data) {
-        const user = await this.#lStorage.save(data);
-        return new User(user)
+    async save(user) {
+        const newUser = await this.#lStorage.save(user.toObject());
+        return new User(newUser)
     }
+
+    async getById(id) {
+        const user = await this.#lStorage.findOne(id);
+
+        if (!user) {
+            return null;
+        }
+
+        return new User(user);
+    }
+
+    async getAll() {
+        const users = await this.#lStorage.findAll();
+        return users.map(user => new User(user));
+    }
+
+    async update(user) {
+        return this.#lStorage.findOneAndUpdate(user.id, user.toObject());
+    }
+
+    async remove(id) {
+        return this.#lStorage.remove(id);
+    }
+
+    async removeAll() {
+        return this.#lStorage.removeAll();
+    }
+
 }
